b64923fa7e8482f7f2e30f1b1f7d04a19c26d56e
Alex Black <alexander.black@dominos.com>
2022-09-21 16:40:10 -0400

    Started sendNotification -- wip

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ const authToken = process.env.AUTH_TOKEN
 const notifyServiceSid = process.env.NOTIFY_SERVICE_SID
 const targetPhoneNumber = process.env.TARGET_PHONE_NUMBER
 
+const httpsOptions = {
+  host: "notify.twilio.com",
+  auth: `${accountSid}:${authToken}`,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept': '*/*'
+  }
+}
+
 const createUserBinding = (identifier, bindingType, targetAddress) => {
+
+  Object.assign(httpsOptions, {
+    path: `/v1/Services/${notifyServiceSid}/Bindings`,
+    method: 'POST'
+  })
+
   const requestData = {
     Identity: identifier,
     BindingType: bindingType,
     Address: targetAddress
-  }
-
-  const httpsOptions = {
-    host: "notify.twilio.com",
-    path: `/v1/Services/${notifyServiceSid}/Bindings`,
-    method: 'POST',
-    auth: `${accountSid}:${authToken}`,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Accept': '*/*'
-    }
-    
   }
 
   let request = https.request(httpsOptions, (res) => {
@@ -37,6 +40,15 @@ const createUserBinding = (identifier, bindingType, targetAddress) => {
   request.on('error', (err) => {
     console.error(`Encoutered an error: ${err.message}`);
   });
+}
+
+const sendNotification = (targetIdentifier, message) => {
+  Object.assign(httpsOptions, {
+    path: `/v1/Services/${notifyServiceSid}/Notifications`,
+    method: 'POST'
+  });
+
+  // Do some other stuff
 }
 
 createUserBinding("blacka-01", "sms", targetPhoneNumber);
